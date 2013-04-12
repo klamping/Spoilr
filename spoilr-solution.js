29917f5263eab6f68e87469c41ce5ae0fb9046f0
Kevin Lamping <klamping@gmail.com>
2013-04-12 00:29:06 -0500

    Added some spoiler text to the spoiler

--- spoilr-solution.js
+++ spoilr-solution.js
@@ -1,4 +1,54 @@
 YUI.add('spoilr', function(Y) {
+
+    // WAIT!!! Do you really want to spoil the spoilr code?
+    // Scrolling down with reveal code that passes all the test
+    // It's just one example of how to do it; There are many others
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // insert your JavaScript here
     var button = Y.one("#show-spoiler");
     var spoiler = Y.one("#spoiler");
