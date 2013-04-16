324078dbda3520fbf459b862e3501633ca9dd6c3
Kevin Lamping <klamping@gmail.com>
2013-04-16 13:09:50 -0400

    minor tweaks

--- spoilr-solution.js
+++ spoilr-solution.js
@@ -1,3 +1,10 @@
+var YUI = YUI || undefined;
+
+if (typeof require == "undefined" && typeof YUI == "undefined") {
+    // we need to pull in YUI using NodeJS's require pattern if it's not already loaded
+    YUI = require('yui').YUI;
+}
+
 YUI.add('spoilr', function(Y) {
 
     // WAIT!!! Do you really want to spoil the spoilr code?
--- spoilr.js
+++ spoilr.js
@@ -1,6 +1,7 @@
 var YUI = YUI || undefined;
 
 if (typeof require == "undefined" && typeof YUI == "undefined") {
+    // we need to pull in YUI using NodeJS's require pattern if it's not already loaded
     YUI = require('yui').YUI;
 }
 
